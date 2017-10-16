3157c0239661aef0b7052a027ea10ef41c5a4dd0
lind1701 <32297190+lind1701@users.noreply.github.com>
2017-10-16 13:07:28 +0200

    Geluidjes toegevoegd LEES DESCRIPTION
    Hallo jongentjes,
    
    Ik heb in de Google Drive 3 bestanden toegevoegd. Dit zijn geluidsbestanden die worden gebruikt voor het main menu. Plaats deze in de bin -> debug -> 'Sound' map. Sound moet wel worden aangemaakt eerst. Als jullie dit hebben gedaan moet het geluid werken.

--- Memory Project Groep 12/Memory Project Groep 12/hoofdmenu.cs
+++ Memory Project Groep 12/Memory Project Groep 12/hoofdmenu.cs
@@ -7,18 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WMPLib;
 
 namespace Memory_Project_Groep_12
 {
     public partial class hoofdmenu : Form
     {
+        WindowsMediaPlayer player = new WindowsMediaPlayer();
         public hoofdmenu()
         {
             InitializeComponent();
+            player.URL = ("Sound/Background.MP3");
+            player.controls.play();
         }
 
         private void play_Click(object sender, EventArgs e)
         {
+            Play();
             this.Hide();
             var game = new game();
             game.Closed += (s, args) => this.Close();
@@ -27,11 +32,13 @@ namespace Memory_Project_Groep_12
 
         private void quit_Click(object sender, EventArgs e)
         {
+            Play();
             System.Windows.Forms.Application.Exit();
         }
 
         private void opties_Click(object sender, EventArgs e)
         {
+            Play();
             this.Hide();
             var opties = new opties();
             opties.Closed += (s, args) => this.Close();
@@ -40,6 +47,7 @@ namespace Memory_Project_Groep_12
 
         private void highscores_Click(object sender, EventArgs e)
         {
+            Play();
             this.Hide();
             var highscores = new Highscores();
             highscores.Closed += (s, args) => this.Close();
@@ -48,6 +56,7 @@ namespace Memory_Project_Groep_12
 
         private void over_Click(object sender, EventArgs e)
         {
+            Play();
             this.Hide();
             var over = new over();
             over.Closed += (s, args) => this.Close();
@@ -57,6 +66,20 @@ namespace Memory_Project_Groep_12
         private void hoofdmenu_Load(object sender, EventArgs e)
         {
 
+        }
+        static public void Play()
+        {
+            string soundfile = "Sound/Click.wav";
+            var sound = new System.Media.SoundPlayer(soundfile);
+            sound.Play();
+        }
+        private void muteUnmute_CheckedChanged(object sender, EventArgs e)
+        {
+            System.Windows.Forms.CheckBox muteUnmute = (System.Windows.Forms.CheckBox)sender;
+
+            muteUnmute.Text = muteUnmute.Checked ? "Un-mute Audio" : muteUnmute.Text = "Mute Audio";
+
+            player.settings.mute = muteUnmute.Checked;
         }
     }
 }
