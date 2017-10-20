0e4f6574823970d4c45b835ebe4d31378605220c
sietseleenstra <leen1702@student.nhl.nl>
2017-10-20 11:49:15 +0200

    versie 0.4.1
    game

--- Memory Project Groep 12/Memory Project Groep 12/game.cs
+++ Memory Project Groep 12/Memory Project Groep 12/game.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Memory_Project_Groep_12
 {
@@ -15,6 +16,28 @@ namespace Memory_Project_Groep_12
         public game()
         {
             InitializeComponent();
+        }
+
+        void Resetimage()
+        {
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox)
+                {
+                    (x as PictureBox).Image = Properties.Resources._0;
+                }
+            }
+        }
+
+        void ResetTags()
+        {
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox)
+                {
+                    (x as PictureBox).Tag = "0";
+                }
+            }
         }
 
         private void terug_Click(object sender, EventArgs e)
@@ -47,6 +70,89 @@ namespace Memory_Project_Groep_12
 
             label1.Text = "Speler 1 = " + spelers.naam1;
             label2.Text = "Speler 2 = " + spelers.naam2;
+
+            Resetimage();
+            //ResetTags();
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            (sender as PictureBox).Image = Image.FromFile((sender as PictureBox).Tag.ToString() + ".png");
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox16_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
